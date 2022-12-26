be5f4d52ee95d8afac069c976c1761df54a51fe6
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2022-12-26 10:36:52 +0530

    completed feature analysis

--- dogvscat.py
+++ dogvscat.py
@@ -1,37 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os 
+import os
 import cv2
 
-data_dir = "dataset/training_set"
-categories = ["cats", "dogs"]
-img_size = 50
-training_data=[]
+
+data = "dataset/training_set"
+categories = ["cats","dogs"]
+
 for category in categories:
-    path = os.path.join(data_dir, category) #path to cats or dogs dir
-    class_num = categories.index(category) #labeling the classes 0 or 1
+    path = os.path.join(data,category)
     for img in os.listdir(path):
-        try:
-            img_array= cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
-            new_array = cv2.resize(img_array, (img_size, img_size))
-            training_data.append([new_array,class_num])
-        except Exception as e:
-            pass
-    #     plt.imshow(img_array,cmap="gray")
-    #     plt.show()
-    #     break
-    # break
+        img_arr = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+        # plt.imshow(img_arr,cmap="gray")
+        # plt.show()
+        break
+    break
 
-#shaping
-print(len(training_data))
+print(img_arr.shape)
 
-# new_array = cv2.resize(img_array, (img_size, img_size))
+#size of image
 
-# plt.imshow(new_array,cmap="gray")
+IMG_SIZE = 75
+
+# new_arr = cv2.resize(img_arr,(IMG_SIZE,IMG_SIZE))
+# plt.imshow(new_arr,cmap="gray")
 # plt.show()
 
 
+training_data =[]
+
+def create_training_data():
+    for category in categories:
+        path = os.path.join(data,category)
+        class_num = categories.index(category)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+                new_arr = cv2.resize(img_arr,(IMG_SIZE,IMG_SIZE))
+                training_data.append([new_arr,class_num])
+            except Exception as e:
+                pass
+create_training_data()
+
+# print(len(training_data))
+
+
+# cat=0
+# dog=0
+# for sample in training_data:
+#     if sample[1] == 0:
+#         cat =cat+1
+#     else:
+#         dog=dog+1
+
+# print(f"cat:{cat} dog:{dog}")
+
+
+#shuffle data
+
 import random
+
 random.shuffle(training_data)
 
 for sample in training_data[:10]:
@@ -40,23 +68,23 @@ for sample in training_data[:10]:
 
 x=[]
 y=[]
+#creating traning dataset
+for features,label in training_data:
+    x.append(features)
+    y.append(label)
 
-for f,l in training_data:
-    x.append(f)
-    y.append(l)
 
-x = np.array(x).reshape(-1, img_size, img_size, 1)
+x=np.array(x).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+
 
 import pickle
 
-pickle_out = open("X.pickle","wb")
-pickle.dump(x, pickle_out)
+pickle_out = open("x.pickle","wb")
+pickle.dump(x,pickle_out)
 pickle_out.close()
 
 pickle_out = open("y.pickle","wb")
-pickle.dump(y, pickle_out)
+pickle.dump(y,pickle_out)
 pickle_out.close()
-
-
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-ans = model.fit(x,y,epochs=3)
+IMG_SIZE = 50
+
+new_arr = cv2.resize(img_arr,(IMG_SIZE,IMG_SIZE))
+plt.imshow(new_arr,cmap="gray")
